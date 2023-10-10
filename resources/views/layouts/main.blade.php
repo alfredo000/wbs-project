9381369ec2d886ea57081cb31ca556246a02c52c
alfredo000 <alfredohutagalung78@gmail.com>
2023-10-10 17:01:32 +0700

    Hide Footer dari Registrasi dan Login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,9 @@
       <div class="container-fluid mt-4">
           @yield('container')
       </div>
-    @include('partials/footer')
+    @if (!isset($hideFooter) || !$hideFooter)
+      @include('partials/footer')
+    @endif
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container">
+    <div class="container" style="color: #2C89BE">
       <a class="navbar-brand" href="/">WBS</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
