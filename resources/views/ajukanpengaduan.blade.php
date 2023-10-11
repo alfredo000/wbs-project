d5985a8ca18a68e6c4d547f0a557d248b194c14f
alfredo000 <alfredohutagalung78@gmail.com>
2023-10-11 17:53:45 +0700

    gambar4

--- resources/views/ajukanpengaduan.blade.php
+++ resources/views/ajukanpengaduan.blade.php
@@ -3,7 +3,10 @@
 @endphp
 @extends('layouts.main')
 @section('container')
-<div class="position-absolute" style="padding-top: 2%;padding-left:1%;">
-    <h6><b>Riwayat Pengaduan</b></h6>
+<div class="container">
+    <div class="position-absolute" style="padding-top: 2%;padding-left:1%;">
+        <h6><b>Riwayat Pengaduan</b></h6>
+    </div>
 </div>
+
 @endsection
